9c52a64fba9eee5eda9fbc8386c4bdff66db6fca
hoangthien <a11718thien@gmail.com>
2023-12-01 16:59:45 +0700

    gpio: check Pin 21 active (why is false)<base on low on P21 GPIO signal>

--- apps/sample/gpioCf3Demo/gpioCf3PinComponent/gpioCf3Pin.c
+++ apps/sample/gpioCf3Demo/gpioCf3PinComponent/gpioCf3Pin.c
@@ -94,73 +94,73 @@ static void Pin21GpioSignal()
  *  Pin-per-service GPIO pin22 as example
  */
 // -------------------------------------------------------------------------------------------------
-// static void Pin22GpioSignal()
-// {
-//     bool value = false;
-//     le_gpioPin22_SetInput(LE_GPIOPIN22_ACTIVE_LOW);
-//     value = le_gpioPin22_Read();
-//     LE_INFO("Pin22 read active: %d", value);
+static void Pin22GpioSignal()
+{
+    bool value = false;
+    le_gpioPin22_SetInput(LE_GPIOPIN22_ACTIVE_LOW);
+    value = le_gpioPin22_Read();
+    LE_INFO("Pin22 read active: %d", value);
 
-//     le_gpioPin22_ChangeEventHandlerRef_t ref = le_gpioPin22_AddChangeEventHandler(LE_GPIOPIN22_EDGE_FALLING, Pin22ChangeCallback, &Pin22, 0);
+    le_gpioPin22_ChangeEventHandlerRef_t ref = le_gpioPin22_AddChangeEventHandler(LE_GPIOPIN22_EDGE_FALLING, Pin22ChangeCallback, &Pin22, 0);
 
-//     // Change the edge setting
-//     le_gpioPin22_SetEdgeSense(LE_GPIOPIN22_EDGE_BOTH);
-//     le_gpioPin22_DisableEdgeSense();
-//     le_gpioPin22_SetEdgeSense(LE_GPIOPIN22_EDGE_RISING);
+    // Change the edge setting
+    le_gpioPin22_SetEdgeSense(LE_GPIOPIN22_EDGE_BOTH);
+    le_gpioPin22_DisableEdgeSense();
+    le_gpioPin22_SetEdgeSense(LE_GPIOPIN22_EDGE_RISING);
 
-//     // Remove the handler
-//     le_gpioPin22_RemoveChangeEventHandler(ref);
-// }
+    // Remove the handler
+    le_gpioPin22_RemoveChangeEventHandler(ref);
+}
 
-// static void PinsReadConfig()
-// {
-//     LE_INFO("Pin 21 active = %s", le_gpioPin21_IsActive()?"true":"false");
+static void PinsReadConfig()
+{
+    LE_INFO("Pin 21 active = %s", le_gpioPin21_IsActive()?"true":"false");
 
-//     le_gpioPin22_Edge_t edge = le_gpioPin22_GetEdgeSense();
-//     if (edge == LE_GPIOPIN22_EDGE_FALLING)
-//     {
-//         LE_INFO("Pin 22 edge sense = falling");
-//     }
-//     else if (edge == LE_GPIOPIN22_EDGE_RISING)
-//     {
-//         LE_INFO("Pin 22 edge sense = rising");
-//     }
-//     else if (edge == LE_GPIOPIN22_EDGE_BOTH)
-//     {
-//         LE_INFO("Pin 22 edge sense = both");
-//     }
-//     else if (edge == LE_GPIOPIN22_EDGE_NONE)
-//     {
-//         LE_INFO("Pin 22 edge sense = none");
-//     }
+    le_gpioPin22_Edge_t edge = le_gpioPin22_GetEdgeSense();
+    if (edge == LE_GPIOPIN22_EDGE_FALLING)
+    {
+        LE_INFO("Pin 22 edge sense = falling");
+    }
+    else if (edge == LE_GPIOPIN22_EDGE_RISING)
+    {
+        LE_INFO("Pin 22 edge sense = rising");
+    }
+    else if (edge == LE_GPIOPIN22_EDGE_BOTH)
+    {
+        LE_INFO("Pin 22 edge sense = both");
+    }
+    else if (edge == LE_GPIOPIN22_EDGE_NONE)
+    {
+        LE_INFO("Pin 22 edge sense = none");
+    }
 
-//     le_gpioPin21_Polarity_t pol = le_gpioPin21_GetPolarity();
-//     if (pol == LE_GPIOPIN21_ACTIVE_HIGH)
-//     {
-//         LE_INFO("Pin 21 polarity = ACTIVE_HIGH");
-//     }
-//     else
-//     {
-//         LE_INFO("Pin 21 polarity = ACTIVE_LOW");
-//     }
+    le_gpioPin21_Polarity_t pol = le_gpioPin21_GetPolarity();
+    if (pol == LE_GPIOPIN21_ACTIVE_HIGH)
+    {
+        LE_INFO("Pin 21 polarity = ACTIVE_HIGH");
+    }
+    else
+    {
+        LE_INFO("Pin 21 polarity = ACTIVE_LOW");
+    }
 
-//     LE_INFO("Pin 21 is input = %s", le_gpioPin21_IsInput()?"true":"false");
-//     LE_INFO("Pin 22 is output = %s", le_gpioPin22_IsOutput()?"true":"false");
+    LE_INFO("Pin 21 is input = %s", le_gpioPin21_IsInput()?"true":"false");
+    LE_INFO("Pin 22 is output = %s", le_gpioPin22_IsOutput()?"true":"false");
 
-//     le_gpioPin21_PullUpDown_t pud = le_gpioPin21_GetPullUpDown();
-//     if (pud == LE_GPIOPIN21_PULL_DOWN)
-//     {
-//         LE_INFO("Pin 21 pull up/down = down");
-//     }
-//     else if (pud == LE_GPIOPIN21_PULL_UP)
-//     {
-//         LE_INFO("Pin 21 pull up/down = up");
-//     }
-//     else
-//     {
-//         LE_INFO("Pin 21 pull up/down = none");
-//     }
-// }
+    le_gpioPin21_PullUpDown_t pud = le_gpioPin21_GetPullUpDown();
+    if (pud == LE_GPIOPIN21_PULL_DOWN)
+    {
+        LE_INFO("Pin 21 pull up/down = down");
+    }
+    else if (pud == LE_GPIOPIN21_PULL_UP)
+    {
+        LE_INFO("Pin 21 pull up/down = up");
+    }
+    else
+    {
+        LE_INFO("Pin 21 pull up/down = none");
+    }
+}
 
 static void SecondCallbackTest()
 {
@@ -174,9 +174,12 @@ COMPONENT_INIT
     LE_INFO("This is sample gpioctl Legato CF3 GPIO app by using le_gpio.api\n");
 
     Pin21GpioSignal();
-   // Pin22GpioSignal();
 
-   // PinsReadConfig();
+    LE_INFO("\n\n\n========== Pin22GpioSignal ===========\n");
+    Pin22GpioSignal();
+
+    LE_INFO("\n\n\n========== PinsReadConfig ===========\n");
+    PinsReadConfig();
 
     // This should abort the test app
     SecondCallbackTest();
